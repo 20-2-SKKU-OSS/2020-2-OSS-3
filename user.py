7410cedd827fa6c3225247a66067f3056df042c9
mancityg <lsh3982@naver.com>
2020-11-29 11:32:44 +0900

    user.py , settings.py 수정

--- user.py
+++ user.py
@@ -1,7 +1,28 @@
-# Class to handle user data
+import pprint
 
 class User:
-    """ class to store user credentials and data"""
+    """ class to store user credentials
+    studentID, loginID, PW should be given at initialization
+    studentID: integer
+    id: string
+    pw: string
 
-    def __init__(self, sid, id, pw):
-        pass
+    헤더랑 쿠키는 인증 정보 저장 및 핸들링
+    classes는 이번학기에 수강중인 과목 id 저장
+    classDatas는 위의 과목 교수님 이름 교과목 명(id 말고 text) 등등 저장 학수번호 저장
+    show는 디버깅용으로 출력하는 메소드
+
+
+    """
+    def __init__(self, studentID: int, id: str, pw: str):
+        self.sid = studentID
+        self.id = id
+        self.pw = pw
+        self.headers = {}
+        self.cookies = {}
+        self.classes = []
+        self.classDatas = {}
+        self.uid = ""
+
+    def show(self):
+        pprint.pprint(vars(self))
